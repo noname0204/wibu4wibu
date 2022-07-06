a863de0b3bd09320472212f9ad1652db5e63ffb3
noname0204 <hoangductai2007@gmail.com>
2022-07-06 10:55:52 +0100

    client: fix ico size; remove useless dom

--- client/src/layouts/components/Header/Header.tsx
+++ client/src/layouts/components/Header/Header.tsx
@@ -9,13 +9,11 @@ import MobileMenu from './MobileMenu';
 const Header: FC = () => {
   return (
     <header className={classes.wrapper}>
-      <div>
-        <Paragraph size={5} className={classes.title}>
-          MyanimeworldZ&nbsp;
-          {/* &nbsp; dùng để đặt thêm khoảng cách ở cuối chữ để fix lỗi của font */}
-        </Paragraph>
-        <MobileMenu />
-      </div>
+      <Paragraph size={5} className={classes.title}>
+        MyanimeworldZ&nbsp;
+        {/* &nbsp; dùng để đặt thêm khoảng cách ở cuối chữ để fix lỗi của font */}
+      </Paragraph>
+      <MobileMenu />
       <Search />
       <div className='ml-0 flex items-center gap-5 lg:ml-16 '>
         <NotificationBell />
